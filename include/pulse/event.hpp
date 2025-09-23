c039d882d80843f0d663669770ec2affc84e0e83
Mike Voltaggio <mike.voltaggio@gmail.com>
2025-09-22 21:59:38 -0500

    - Added EventConnection class - Updated Event class to hold a list of pair<EventConnect, std::function> instead of just std::function -

--- include/pulse/event.hpp
+++ include/pulse/event.hpp
@@ -2,6 +2,7 @@
 #define PULSE_EVENT_HPP
 #include <functional>
 #include <vector>
+#include <memory>
 
 namespace pulse
 {
@@ -13,14 +14,39 @@ namespace pulse
 	class Event
 	{
 	public:
-		void addListener(std::function<void(Args...)> fn);
+		class EventConnection
+		{
+			friend Event;
 
-		void invoke(Args... args);
-		void clear();
+			inline void connect(Event* owner, int newID)
+			{
+				_eventOwner = owner;
+				_id = newID;
+			}
+
+			inline void disconnect()
+			{
+				if (_eventOwner != nullptr) {
+					_eventOwner->disconnectListener(_id);
+				}
+			}
+
+			int _id;
+			Event* _eventOwner;
+		};
+
+
+		EventConnection addListener(std::function<void(Args...)> fn);
+		inline void invoke(Args... args);
+		inline void clear();
 
 
 	private:
-		std::vector <std::function<void(Args...)>> listeners;
+
+		bool disconnectListener(int removeID);
+
+		std::vector <std::pair<EventConnection, std::function<void(Args...)>>> _listeners;
+		int _currentID;
 	};
 
 	// IMPLEMENTATION
@@ -32,19 +58,34 @@ namespace pulse
 	/// <param name="...args"></param>
 	template<typename ...Args>
 	void Event<Args...>::invoke(Args... args) {
-		for (auto fn : listeners) {
-			fn(std::forward<Args>(args)...);
+		for (auto fn : _listeners) {
+			fn.second(std::forward<Args>(args)...);
 		}
 	}
 
 	template<typename ...Args>
-	void Event<Args...>::addListener(std::function<void(Args...)> fn) {
-		listeners.push_back(fn);
+	pulse::Event<Args...>::EventConnection Event<Args...>::addListener(std::function<void(Args...)> fn) {
+		++_currentID;
+		Event<Args...>::EventConnection conn;
+		conn.connect(this, _currentID);
+		_listeners.push_back(std::make_pair(conn, std::move(fn)));
+		return conn;
 	}
 
 	template<typename ...Args>
 	void Event<Args...>::clear() {
-		listeners.clear();
+		_listeners.clear();
+		_currentID = 0;
+	}
+
+	template<typename ...Args>
+	bool Event<Args...>::disconnectListener(int removeID)
+	{
+		auto iter = std::remove_if(_listeners.begin(), _listeners.end(), [=](auto& pair) {
+			return (pair.first == removeID);
+			});
+		if(iter)
+			_listeners.erase(iter);
 	}
 
 
